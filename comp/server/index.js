d0a1ab9a840d2ecddfc8ac6c9bdb686bff61a3e8
Travis Meamber <tmeamber623@gmail.com>
2017-12-18 10:44:26 -0700

    added things but nothing to check off

--- comp/server/index.js
+++ comp/server/index.js
@@ -1,1 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const app = express();
+const PORT = 3005;
 
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+massive(process.env.CONNECTION_STRING).then((db) => {
+    console.log("DB connected")
+    app.set('db', db);
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
